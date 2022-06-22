a83f72b3ea688d07b2c061ad546be3b27f9c7c71
Niek Candaele <niek.candaele@rightcrowd.com>
2022-06-22 09:58:40 +0200

    add some gauges that can be used for scaling tryouts

--- src/metrics.js
+++ src/metrics.js
@@ -1,5 +1,25 @@
-import { collectDefaultMetrics, register } from 'prom-client';
+import { collectDefaultMetrics, register, Gauge } from 'prom-client';
+
 collectDefaultMetrics();
+
+let oscValue = 0;
+
+new Gauge({
+  name: 'oscillating_gauge',
+  help: 'This gauge oscillates from 0 to 10',
+  async collect() {
+    oscValue = (oscValue + 1) % 11;
+    this.set(oscValue);
+  }
+});
+
+new Gauge({
+  name: 'random_gauge',
+  help: 'This gauge is a random number between 0 and 100',
+  async collect() {
+    this.set(Math.floor(Math.random() * 100));
+  }
+})
 
 export async function metrics(ctx) {
   ctx.body = await register.metrics();
